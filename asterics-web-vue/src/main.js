e76d540fae5b4a9c8bd2f025514d6771133302b9
Alija Sabic <sabic.alija@gmail.com>
2018-08-14 15:36:52 +0200

    Migrate fontawesome icon loading from main.js into separate file (src/libraries)

--- asterics-web-vue/src/main.js
+++ asterics-web-vue/src/main.js
@@ -8,39 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // FontAwesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-// eslint-disable-next-line
-import {
-  faCircle,
-  faBook,
-  faCode,
-  faEraser,
-  faQuestionCircle,
-  faUniversalAccess,
-  faDownload,
-  faSearch
-} from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faWordpress } from "@fortawesome/free-brands-svg-icons";
-import {
-  FontAwesomeIcon,
-  FontAwesomeLayers,
-  FontAwesomeLayersText
-} from "@fortawesome/vue-fontawesome";
-library.add(
-  faCircle,
-  faBook,
-  faCode,
-  faEraser,
-  faQuestionCircle,
-  faUniversalAccess,
-  faDownload,
-  faSearch,
-  faGithub,
-  faWordpress
-);
-Vue.component("font-awesome-icon", FontAwesomeIcon);
-Vue.component("font-awesome-layers", FontAwesomeLayers);
-Vue.component("font-awesome-layers-text", FontAwesomeLayersText);
+import "@/libraries/icons";
 
 // Components
 import App from "./App.vue";
