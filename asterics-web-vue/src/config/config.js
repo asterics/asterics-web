7272dfcb35cd347577d396e3866cc65bf62263b6
Alija Sabic <sabic.alija@gmail.com>
2019-02-04 23:56:54 +0100

    Add central configuration

---
+++ asterics-web-vue/src/config/config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+let envConfigPath = path.join(process.cwd(), "src", "config", ".env"),
+  schemaPath = path.join(process.cwd(), "src", "config", "schema.json");
+
+require("dotenv").config({ path: envConfigPath });
+const convict = require("convict"),
+  config = convict(schemaPath),
+  { getReferenceInPath } = require("@asterics/git-tools");
+
+/* Load configuration */
+config.load({
+  env: "production",
+  submodules: [
+    {
+      name: "asterics-docs",
+      destination: path.join(process.cwd(), "src", "external", "asterics-docs"),
+      reference: getReferenceInPath(process.cwd(), "asterics-docs"),
+      branch: "sprint-201901-load-ext-resources"
+    },
+    {
+      name: "WebACS",
+      destination: path.join(process.cwd(), "src", "external", "webacs"),
+      reference: getReferenceInPath(process.cwd(), "WebACS"),
+      branch: "master"
+    }
+  ]
+});
+
+/* Validate configuration with loaded schema */
+config.validate({ allowed: "warn" });
+
+module.exports = config;
