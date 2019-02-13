1fd26ba2c8f6064a3daa9ea2d02aa264f7f51d5d
Alija Sabic <sabic.alija@gmail.com>
2019-02-13 12:31:48 +0100

    Update asterics-web config (submodule key destination -> location)

--- asterics-web-vue/src/scripts/setup.js
+++ asterics-web-vue/src/scripts/setup.js
@@ -15,16 +15,16 @@ buildAstericsDocs();
 function buildAstericsDocs() {
   const asterics_docs = config.get("submodules").find(({ name }) => name === "asterics-docs");
   execute({
-    cmd: `npm install --prefix ${asterics_docs.destination}`,
-    success: `npm dependencies installed in ${asterics_docs.destination}`,
-    error: `failed installing npm dependencies in ${asterics_docs.destination}`,
+    cmd: `npm install --prefix ${asterics_docs.location}`,
+    success: `npm dependencies installed in ${asterics_docs.location}`,
+    error: `failed installing npm dependencies in ${asterics_docs.location}`,
     verbose: config.get("verbose")
   });
 
   execute({
-    cmd: `npm run-script setup --prefix ${asterics_docs.destination}`,
-    success: `build asterics-docs in ${asterics_docs.destination}`,
-    error: `failed building asterics-docs in ${asterics_docs.destination}`,
+    cmd: `npm run-script setup --prefix ${asterics_docs.location}`,
+    success: `build asterics-docs in ${asterics_docs.location}`,
+    error: `failed building asterics-docs in ${asterics_docs.location}`,
     verbose: config.get("verbose"),
     env: { ENDPOINT: "docs" }
   });
