507ce0137d3bd15541698ba5dc3a78422f99d443
Alija Sabic <sabic.alija@gmail.com>
2019-03-27 18:30:24 +0100

    release: correct zip path comment

--- asterics-web-vue/src/scripts/semantic-release/prepare.js
+++ asterics-web-vue/src/scripts/semantic-release/prepare.js
@@ -2,12 +2,12 @@ const { lstatSync, readdirSync, symlinkSync } = require("fs");
 
 const version = process.argv[2];
 
-/* Search `docs.zip` in root folder */
+/* Search `asterics-web.zip` in root folder */
 const release = readdirSync(process.cwd())
   .filter(e => !lstatSync(e).isDirectory())
   .filter(e => e.match(/^asterics-web.zip$/));
 
-/* Rename if `docs.zip` found */
+/* Rename if `asterics-web.zip` found */
 if (release.length > 0) {
   symlinkSync("asterics-web.zip", `asterics-web-v${version}.zip`);
 }
